9d400c5de59a10e6b6df989151665ce1bc58b6b2
isiahTo <pgmr.isiah@gmail.com>
2018-06-22 20:57:07 -0700

    Create basic form and loading functionality, add Emgu library

--- EmguTest/Form1.Designer.cs
+++ EmguTest/Form1.Designer.cs
@@ -29,11 +29,74 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.imgBox = new Emgu.CV.UI.ImageBox();
+            this.btn_reset = new System.Windows.Forms.Button();
+            this.btn_rmat = new System.Windows.Forms.Button();
+            this.btn_save = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imgBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imgBox
+            // 
+            this.imgBox.Location = new System.Drawing.Point(12, 12);
+            this.imgBox.Name = "imgBox";
+            this.imgBox.Size = new System.Drawing.Size(260, 237);
+            this.imgBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.imgBox.TabIndex = 2;
+            this.imgBox.TabStop = false;
+            // 
+            // btn_reset
+            // 
+            this.btn_reset.Location = new System.Drawing.Point(12, 255);
+            this.btn_reset.Name = "btn_reset";
+            this.btn_reset.Size = new System.Drawing.Size(75, 23);
+            this.btn_reset.TabIndex = 3;
+            this.btn_reset.Text = "Reset";
+            this.btn_reset.UseVisualStyleBackColor = true;
+            this.btn_reset.Click += new System.EventHandler(this.btn_reset_Click);
+            // 
+            // btn_rmat
+            // 
+            this.btn_rmat.Location = new System.Drawing.Point(93, 255);
+            this.btn_rmat.Name = "btn_rmat";
+            this.btn_rmat.Size = new System.Drawing.Size(75, 23);
+            this.btn_rmat.TabIndex = 4;
+            this.btn_rmat.Text = "RMAT";
+            this.btn_rmat.UseVisualStyleBackColor = true;
+            this.btn_rmat.Click += new System.EventHandler(this.btn_rmat_Click);
+            // 
+            // btn_save
+            // 
+            this.btn_save.Location = new System.Drawing.Point(197, 255);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(75, 23);
+            this.btn_save.TabIndex = 5;
+            this.btn_save.Text = "Save";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 298);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.btn_rmat);
+            this.Controls.Add(this.btn_reset);
+            this.Controls.Add(this.imgBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imgBox)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Emgu.CV.UI.ImageBox imgBox;
+        private System.Windows.Forms.Button btn_reset;
+        private System.Windows.Forms.Button btn_rmat;
+        private System.Windows.Forms.Button btn_save;
     }
 }
 
--- EmguTest/Form1.cs
+++ EmguTest/Form1.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +10,129 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace EmguTest
 {
     public partial class Form1 : Form
     {
+        private static string IMAGE_PATH = @"C:\Users\Isiah\Pictures\cotn\templates\Cadence.png";
+
+        private static Image<Rgba, byte> DEFAULT_IMAGE = new Image<Rgba, byte>(IMAGE_PATH);
+
         public Form1()
         {
             InitializeComponent();
+            InitImage();
+            //LoadImageData();
+            //RunTemplateMatching();
+        }
+
+        private void InitImage()
+        {
+            SetImage(DEFAULT_IMAGE);
+        }
+
+        private void SetImage(IImage src)
+            => imgBox.Image = src;
+
+       
+
+        private void ConvertBlackToTransparent()
+        {
+            var gray = new Image<Gray, byte>(DEFAULT_IMAGE.Bitmap);
+            var alphaImg = gray.ThresholdBinary(new Gray(10), new Gray(255));
+            var alpha = alphaImg.Mat;
+
+            var mats = DEFAULT_IMAGE.Mat.Split();
+
+            Mat[] rgba = { mats[0], mats[1], mats[2], alpha  };
+
+            var vMat = new VectorOfMat(rgba);
+            var final = new Mat(gray.Rows, gray.Cols, Emgu.CV.CvEnum.DepthType.Cv8U, 4);
+            CvInvoke.Merge(vMat, final);
+
+            imgBox.Image = final.ToImage<Rgba, byte>();
+
+        }
+
+        private void ConvertBlackToTransparent(Bitmap image)
+        {
+            //Mat src = new Mat(new Size(image.Width, image.Height),
+            //                    Emgu.CV.CvEnum.DepthType.Cv8U, 4);
+
+            var src = new Image<Rgb, byte>(image);
+            var graySrc = src.Convert<Gray, byte>();
+
+            // var alpha = graySrc.ThresholdBinary(new Gray(100), new Gray(255));
+
+            var srcMat = src.Mat;
+            var finalMat = new Mat(image.Height, image.Width, Emgu.CV.CvEnum.DepthType.Cv8U, 4);
+            var tmp = new Mat(image.Height, image.Width, Emgu.CV.CvEnum.DepthType.Cv8U, 1);
+            var alpha = new Mat(image.Height, image.Width, Emgu.CV.CvEnum.DepthType.Cv8U, 1);
+
+            CvInvoke.CvtColor(srcMat, tmp, Emgu.CV.CvEnum.ColorConversion.Bgr2Gray);
+            imgBox.Image = tmp.ToImage<Rgba, byte>();
+
+            CvInvoke.Threshold(tmp, alpha, 100, 255, Emgu.CV.CvEnum.ThresholdType.Binary);
+            imgBox.Image = alpha.ToImage<Rgba, byte>();
+
+
+            var rgb = new VectorOfMat(3);
+
+            CvInvoke.Split(srcMat, rgb);
+
+            Mat[] rgba = { rgb[0], rgb[1], rgb[2], alpha };
+
+            VectorOfMat rgbaMat = new VectorOfMat(rgba);
+
+            CvInvoke.Merge(rgbaMat, finalMat);
+
+            imgBox.Image = finalMat.ToImage<Rgba, byte>();
+
+            //Mat m = new Mat(image.Height, image.Width, Emgu.CV.CvEnum.DepthType.Cv8U, 4);
+
+            //m.PushBack(src[0].Mat);
+            //m.PushBack(src[1].Mat);
+            //m.PushBack(src[2].Mat);
+            //m.PushBack(alpha.Mat);
+
+            //imgBox.Image = m.ToImage<Rgba, byte>();
+        }
+
+        private void LoadImageData()
+        {
+            var src = new Bitmap(Image.FromFile(IMAGE_PATH));
+            ConvertBlackToTransparent(src);
+            // imgBox.Image = new Image<Rgba, byte>(src);
+        }
+
+        private void btn_reset_Click(object sender, EventArgs e)
+        {
+            InitImage();
+        }
+
+        private void btn_rmat_Click(object sender, EventArgs e)
+        {
+            ConvertBlackToTransparent();
+        }
+
+
+        /** Testing:
+         * 1. Create a folder for testing images
+         * 2. Create a transparent test image
+         * 3. Test the save functionality
+         */
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            var d = new SaveFileDialog();
+            
+
+            if (d.ShowDialog() == DialogResult.OK)
+            {
+                var path = d.FileName;
+                DEFAULT_IMAGE.Save(path);
+            }
         }
     }
 }
